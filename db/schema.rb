5fa2b1a8717eb0116260450461f5da68eee1b7d7
jordanhudgens <jordan@devcamp.com>
2019-05-04 18:37:03 -0700

    Added color to the team table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_013312) do
+ActiveRecord::Schema.define(version: 2019_05_05_013627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_013312) do
     t.string "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
   create_table "users", force: :cascade do |t|
